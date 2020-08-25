269d2c5224a66463b18944de116f4d321b9d5e35
Namnamseo <Namnamseo+github@gmail.com>
2020-08-25 21:03:44 +0900

    Update template code

--- template.cpp
+++ template.cpp
@@ -3,13 +3,9 @@ using namespace std;
 typedef long long ll;
 typedef pair<int,int> pp;
 typedef pair<ll,ll> pll;
-void read(int& x){ scanf("%d",&x); }
-void read(ll& x){ scanf("%lld",&x); }
-void read(pp& x){ scanf("%d%d",&x.first, &x.second); }
-void read(pll& x){ scanf("%lld%lld",&x.first, &x.second); }
-template<typename T,typename... Args>
-void read(T& a,Args&... b){ read(a); read(b...); }
 void cppio(){ ios_base::sync_with_stdio(0); cin.tie(0); }
+template<typename T,typename U>
+istream& operator>>(istream& i, pair<T,U>& p) { i >> p.first >> p.second; return i; }
 #define all(x) (x).begin(),(x).end()
 #define pb push_back
 #define eb emplace_back
@@ -18,6 +14,7 @@ void cppio(){ ios_base::sync_with_stdio(0); cin.tie(0); }
 #define rep(i,n) for(int i = 0; i < (n); ++i)
 #define rrep(i,n) for(int i = 1; i <= (n); ++i)
 #define sz(x) (int)(x).size()
+#define XY(t) t.x, t.y
 
 int main()
 {
